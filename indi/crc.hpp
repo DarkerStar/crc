721990f862dd246d41c19c4b45e6bf29d0af81e6
Mark A. Gibbs <indi.in.the.wired@gmail.com>
2016-09-20 18:07:28 -0400

    Common polynomials

--- indi/crc.hpp
+++ indi/crc.hpp
@@ -17,10 +17,39 @@
 #ifndef INDI_INC_CRC_
 #define INDI_INC_CRC_
 
+#include <cstdint>
+
 namespace indi {
+namespace crc {
 
+namespace polynomials {
 
+// Common 16-bit CRC polynomials
+// "CRC16" usually means the IBM polynomial.
+constexpr auto crc16             = std::uint_fast16_t(0x8005u);
+constexpr auto crc16_ibm         = std::uint_fast16_t(0x8005u);
+constexpr auto crc16_ccitt       = std::uint_fast16_t(0x1021u);
+constexpr auto crc16_t10_dif     = std::uint_fast16_t(0x8BB7u);
+constexpr auto crc16_dnp         = std::uint_fast16_t(0x3D65u);
+constexpr auto crc16_dect        = std::uint_fast16_t(0x0589u);
+constexpr auto crc16_arinc       = std::uint_fast16_t(0xA02Bu);
+constexpr auto crc16_chakravarty = std::uint_fast16_t(0x2F15u);
 
+// Common 32-bit crc polynomials
+// "CRC32" almost always means the 0x04C11DB7 polynomial.
+constexpr auto crc32  = std::uint_fast32_t(0x04C11DB7uL);
+constexpr auto crc32c = std::uint_fast32_t(0x1EDC6F41uL);
+constexpr auto crc32k = std::uint_fast32_t(0x741B8CD7uL);
+constexpr auto crc32q = std::uint_fast32_t(0x814141ABuL);
+
+// Common 32-bit crc polynomials
+// Not sure which polynomial should be considered "CRC64".
+constexpr auto crc64_iso  = std::uint_fast64_t(0x000000000000001BuLL);
+constexpr auto crc64_ecma = std::uint_fast64_t(0x42F0E1EBA9EA3693uLL);
+
+} // namespace polynomials
+
+} // namespace crc
 } // namespace indi
 
 #endif // include guard
---
+++ test/polynomials.cpp
@@ -0,0 +1,56 @@
+/* This file is part of indi-crc.
+ * 
+ * indi-crc is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * indi-crc is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with indi-crc.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "indi/crc.hpp"
+
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(polynomials_suite)
+
+BOOST_AUTO_TEST_CASE(crc16_polynomial_values)
+{
+	BOOST_CHECK_EQUAL(indi::crc::polynomials::crc16_ibm, 0x8005u);
+	BOOST_CHECK_EQUAL(indi::crc::polynomials::crc16_ccitt, 0x1021u);
+	BOOST_CHECK_EQUAL(indi::crc::polynomials::crc16_t10_dif, 0x8BB7u);
+	BOOST_CHECK_EQUAL(indi::crc::polynomials::crc16_dnp, 0x3D65u);
+	BOOST_CHECK_EQUAL(indi::crc::polynomials::crc16_dect, 0x0589u);
+	BOOST_CHECK_EQUAL(indi::crc::polynomials::crc16_arinc, 0xA02Bu);
+	BOOST_CHECK_EQUAL(indi::crc::polynomials::crc16_chakravarty,
+		0x2F15u);
+	
+	// "CRC16" by default is CRC16-IBM.
+	BOOST_CHECK_EQUAL(indi::crc::polynomials::crc16,
+		indi::crc::polynomials::crc16_ibm);
+}
+
+BOOST_AUTO_TEST_CASE(crc32_polynomial_values)
+{
+	BOOST_CHECK_EQUAL(indi::crc::polynomials::crc32, 0x04C11DB7uL);
+	BOOST_CHECK_EQUAL(indi::crc::polynomials::crc32c, 0x1EDC6F41uL);
+	BOOST_CHECK_EQUAL(indi::crc::polynomials::crc32k, 0x741B8CD7uL);
+	BOOST_CHECK_EQUAL(indi::crc::polynomials::crc32q, 0x814141ABuL);
+}
+
+BOOST_AUTO_TEST_CASE(crc64_polynomial_values)
+{
+	BOOST_CHECK_EQUAL(indi::crc::polynomials::crc64_iso,
+		0x000000000000001BuLL);
+	BOOST_CHECK_EQUAL(indi::crc::polynomials::crc64_ecma,
+		0x42F0E1EBA9EA3693uLL);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
