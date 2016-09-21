8c22d5ecdc88d8656f26fe7d2407a220cf201de1
Mark A. Gibbs <indi.in.the.wired@gmail.com>
2016-09-20 20:42:22 -0400

    Added CRC32-IEEE and CRC32-ANSI

--- indi/crc.hpp
+++ indi/crc.hpp
@@ -24,7 +24,7 @@ namespace crc {
 
 namespace polynomials {
 
-// Common 16-bit CRC polynomials
+// Common 16-bit CRC polynomials.
 // "CRC16" usually means the IBM polynomial.
 constexpr auto crc16             = std::uint_fast16_t(0x8005u);
 constexpr auto crc16_ibm         = std::uint_fast16_t(0x8005u);
@@ -35,14 +35,16 @@ constexpr auto crc16_dect        = std::uint_fast16_t(0x0589u);
 constexpr auto crc16_arinc       = std::uint_fast16_t(0xA02Bu);
 constexpr auto crc16_chakravarty = std::uint_fast16_t(0x2F15u);
 
-// Common 32-bit crc polynomials
-// "CRC32" almost always means the 0x04C11DB7 polynomial.
-constexpr auto crc32  = std::uint_fast32_t(0x04C11DB7uL);
-constexpr auto crc32c = std::uint_fast32_t(0x1EDC6F41uL);
-constexpr auto crc32k = std::uint_fast32_t(0x741B8CD7uL);
-constexpr auto crc32q = std::uint_fast32_t(0x814141ABuL);
+// Common 32-bit crc polynomials.
+constexpr auto crc32_ansi = std::uint_fast32_t(0x04C11DB7uL);
+constexpr auto crc32_ieee = std::uint_fast32_t(0x04C11DB7uL);
+constexpr auto crc32c     = std::uint_fast32_t(0x1EDC6F41uL);
+constexpr auto crc32k     = std::uint_fast32_t(0x741B8CD7uL);
+constexpr auto crc32q     = std::uint_fast32_t(0x814141ABuL);
+// "CRC32" almost always means CRC32-IEEE.
+constexpr auto crc32 = crc32_ieee;
 
-// Common 32-bit crc polynomials
+// Common 32-bit crc polynomials.
 // Not sure which polynomial should be considered "CRC64".
 constexpr auto crc64_iso  = std::uint_fast64_t(0x000000000000001BuLL);
 constexpr auto crc64_ecma = std::uint_fast64_t(0x42F0E1EBA9EA3693uLL);
--- test/polynomials.cpp
+++ test/polynomials.cpp
@@ -23,34 +23,38 @@ BOOST_AUTO_TEST_SUITE(polynomials_suite)
 
 BOOST_AUTO_TEST_CASE(crc16_polynomial_values)
 {
-	BOOST_CHECK_EQUAL(indi::crc::polynomials::crc16_ibm, 0x8005u);
-	BOOST_CHECK_EQUAL(indi::crc::polynomials::crc16_ccitt, 0x1021u);
-	BOOST_CHECK_EQUAL(indi::crc::polynomials::crc16_t10_dif, 0x8BB7u);
-	BOOST_CHECK_EQUAL(indi::crc::polynomials::crc16_dnp, 0x3D65u);
-	BOOST_CHECK_EQUAL(indi::crc::polynomials::crc16_dect, 0x0589u);
-	BOOST_CHECK_EQUAL(indi::crc::polynomials::crc16_arinc, 0xA02Bu);
-	BOOST_CHECK_EQUAL(indi::crc::polynomials::crc16_chakravarty,
+	namespace polynomials = indi::crc::polynomials;
+	BOOST_CHECK_EQUAL(polynomials::crc16_ibm, 0x8005u);
+	BOOST_CHECK_EQUAL(polynomials::crc16_ccitt, 0x1021u);
+	BOOST_CHECK_EQUAL(polynomials::crc16_t10_dif, 0x8BB7u);
+	BOOST_CHECK_EQUAL(polynomials::crc16_dnp, 0x3D65u);
+	BOOST_CHECK_EQUAL(polynomials::crc16_dect, 0x0589u);
+	BOOST_CHECK_EQUAL(polynomials::crc16_arinc, 0xA02Bu);
+	BOOST_CHECK_EQUAL(polynomials::crc16_chakravarty,
 		0x2F15u);
 	
 	// "CRC16" by default is CRC16-IBM.
-	BOOST_CHECK_EQUAL(indi::crc::polynomials::crc16,
-		indi::crc::polynomials::crc16_ibm);
+	BOOST_CHECK_EQUAL(polynomials::crc16, polynomials::crc16_ibm);
 }
 
 BOOST_AUTO_TEST_CASE(crc32_polynomial_values)
 {
-	BOOST_CHECK_EQUAL(indi::crc::polynomials::crc32, 0x04C11DB7uL);
-	BOOST_CHECK_EQUAL(indi::crc::polynomials::crc32c, 0x1EDC6F41uL);
-	BOOST_CHECK_EQUAL(indi::crc::polynomials::crc32k, 0x741B8CD7uL);
-	BOOST_CHECK_EQUAL(indi::crc::polynomials::crc32q, 0x814141ABuL);
+	namespace polynomials = indi::crc::polynomials;
+	BOOST_CHECK_EQUAL(polynomials::crc32_ansi, 0x04C11DB7uL);
+	BOOST_CHECK_EQUAL(polynomials::crc32_ieee, 0x04C11DB7uL);
+	BOOST_CHECK_EQUAL(polynomials::crc32c, 0x1EDC6F41uL);
+	BOOST_CHECK_EQUAL(polynomials::crc32k, 0x741B8CD7uL);
+	BOOST_CHECK_EQUAL(polynomials::crc32q, 0x814141ABuL);
+	
+	// "CRC32" by default is CRC31-IEEE.
+	BOOST_CHECK_EQUAL(polynomials::crc32, polynomials::crc32_ieee);
 }
 
 BOOST_AUTO_TEST_CASE(crc64_polynomial_values)
 {
-	BOOST_CHECK_EQUAL(indi::crc::polynomials::crc64_iso,
-		0x000000000000001BuLL);
-	BOOST_CHECK_EQUAL(indi::crc::polynomials::crc64_ecma,
-		0x42F0E1EBA9EA3693uLL);
+	namespace polynomials = indi::crc::polynomials;
+	BOOST_CHECK_EQUAL(polynomials::crc64_iso, 0x000000000000001BuLL);
+	BOOST_CHECK_EQUAL(polynomials::crc64_ecma, 0x42F0E1EBA9EA3693uLL);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
